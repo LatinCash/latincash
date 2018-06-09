c6d74fd82d88fe8f5579155f6dcfb8b10e5e949c
LatinCash <34460610+LatinCash@users.noreply.github.com>
2018-06-09 02:25:25 -0300

    Update DatabaseBlockchainCache.cpp

--- src/CryptoNoteCore/DatabaseBlockchainCache.cpp
+++ src/CryptoNoteCore/DatabaseBlockchainCache.cpp
@@ -302,6 +302,10 @@ public:
   void increment() {
     ++globalOutputIndex;
   }
+  
+  void decrement() {
+    --globalOutputIndex;
+  }
 
   void advance(difference_type n) {
     assert(n >= -static_cast<difference_type>(globalOutputIndex));
